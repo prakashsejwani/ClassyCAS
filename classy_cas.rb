4aba56441e4e06c3207bf57d7ab25969700be231
prakash <prakash@econify.com>
2010-09-28 13:06:30 +0530

    Merged master with github.com:Econify/ClassyCAS

--- classy_cas.rb
+++ classy_cas.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+
 require 'redis'
 require 'haml'
 require 'addressable/uri'
@@ -9,10 +10,12 @@ require 'lib/login_ticket'
 require 'lib/proxy_ticket'
 require 'lib/service_ticket'
 require 'lib/ticket_granting_ticket'
+require 'lib/user_store'
 
 before do
-  @redis = Redis.new
+    @redis ||= Redis.new
 end
+
 
 get "/login" do
   @service_url = Addressable::URI.parse(params[:service])
@@ -20,6 +23,7 @@ get "/login" do
   @gateway = [true, "true", "1", 1].include?(params[:gateway])
   
   if @renew
+    @login_ticket = LoginTicket.create!(@redis)
     haml :login
   elsif @gateway
     if @service_url
@@ -33,6 +37,7 @@ get "/login" do
         redirect @service_url.to_s, 303
       end
     else
+      @login_ticket = LoginTicket.create!(@redis)
       haml :login
     end
   else
@@ -47,6 +52,7 @@ get "/login" do
         return haml :already_logged_in
       end
     else
+      @login_ticket = LoginTicket.create!(@redis)
       haml :login
     end
   end
@@ -63,9 +69,11 @@ post "/login" do
   # Spec is undefined about what to do without these params, so redirecting to credential requestor
   redirect "/login", 303 unless username && password && login_ticket
   
-  if username == "quentin" && password == "testpassword"
+  if UserStore.authenticate(username, password)
     if service_url && !warn
-      redirect service_url, 303
+      st = ServiceTicket.new(service_url, username)
+      st.save!(@redis)
+      redirect service_url + "?ticket=#{st.ticket}", 303
     else
       haml :logged_in
     end
@@ -74,13 +82,13 @@ post "/login" do
   end
 end
 
-get "/serviceValidate" do
+get %r{(proxy|service)Validate} do
   service_url = params[:service]
-  
+  ticket = params[:ticket]
   # proxy_gateway = params[:pgtUrl]
   # renew = params[:renew]
   
-  xml = if params[:service] && params[:ticket]
+  xml = if service_url && ticket
     if service_ticket
       if service_ticket.valid_for_service?(service_url)
         render_validation_success service_ticket.username
@@ -95,7 +103,7 @@ get "/serviceValidate" do
   end
   
   content_type :xml
-  xml.to_xml
+  xml
 end
 
 private
@@ -108,23 +116,35 @@ def login_ticket
 end
 
 def service_ticket
-  @service_ticket ||= ServiceTicket.validate!(params[:ticket], @redis)
+  @service_ticket ||= ServiceTicket.find!(params[:ticket], @redis)
 end
 
 def render_validation_error(code)
-  Nokogiri::XML::Builder.new do |xml|
-    xml.serviceResponse("xmlns" => "http://www.yale.edu/tp/cas") {
-      xml.authenticationFailure(:code => code.to_s.upcase)
+  xml = Nokogiri::XML::Builder.new do |xml|
+    xml.serviceResponse("xmlns:cas" => "http://www.yale.edu/tp/cas") {
+      xml['cas'].authenticationFailure(:code => code.to_s.upcase)
     }
   end
+  namespace_hack(xml)
 end
 
 def render_validation_success(username)
-  Nokogiri::XML::Builder.new do |xml|
-    xml.serviceResponse("xmlns" => "http://www.yale.edu/tp/cas") {
-      xml.authenticationSuccess {
-        xml.user username
+  xml = Nokogiri::XML::Builder.new do |xml|
+    xml.serviceResponse("xmlns:cas" => "http://www.yale.edu/tp/cas") {
+      xml['cas'].authenticationSuccess {
+        xml['cas'].user username
       }
     }
   end
+  namespace_hack(xml)
+end
+
+
+
+#Nokogiri will not allow a namespace to be used before
+#It's declared, why this is I don't know.
+def namespace_hack(xml)  
+  result = xml.to_xml
+  result = result.gsub(/serviceResponse/, 'cas:serviceResponse')
+  result
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,20 @@ require 'rubygems'
 require 'shoulda'
 require 'ruby-debug'
 require 'redis'
+require "rack/test"
 require 'webrat'
 
 Webrat.configure do |config|
   config.mode = :rack
 end
 
+Shoulda::ClassMethods.module_eval do
+  alias :must :should
+  alias :may :should
+end
+
+module Test::Unit::Assertions
+  def assert_false(object, message="")
+    assert_equal(false, object, message)
+  end
+end
